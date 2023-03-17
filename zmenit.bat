da9167cb97545d631aa4ede578f405832702a655
Thisík <67510205+Thisik@users.noreply.github.com>
2023-03-17 10:23:53 +0100

    Add files via upload

---
+++ zmenit.bat
@@ -0,0 +1,25 @@
+@echo off
+title Menic pisma
+echo Jdete do nastaveni a zkopirujte nazev pisma, ktery se vam bude libit - napr. Jokerman.
+echo Az budes mit vybrano a zkopirovano, stiskni zde libovolnou klavesu
+pause > nul
+set /p pismo=Jake chces pismo?
+echo Stiskni cokoliv pro zmeneni pisma.
+pause > nul
+echo Windows Registry Editor Version 5.00 > pismo.reg
+echo. >> pismo.reg
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts] >> pismo.reg
+echo "Segoe UI (TrueType)"="" >> pismo.reg
+echo "Segoe UI Bold (TrueType)"="" >> pismo.reg
+echo "Segoe UI Bold Italic (TrueType)"="" >> pismo.reg
+echo "Segoe UI Italic (TrueType)"="" >> pismo.reg
+echo "Segoe UI Light (TrueType)"="" >> pismo.reg
+echo "Segoe UI Semibold (TrueType)"="" >> pismo.reg
+echo "Segoe UI Symbol (TrueType)"="" >> pismo.reg
+echo. >> pismo.reg
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes] >> pismo.reg
+echo. >> pismo.reg
+echo "Segoe UI"="%pismo%" >> pismo.reg
+echo HOTOVO. NYNI NAJDETE SOUBOR PISMO.REG A OTEVRETE HO. POTVRDTE A RESTARTUJTE POCITAC PRO APLIKOVANI ZMEN!
+pause
+exit
